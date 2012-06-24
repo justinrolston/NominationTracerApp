e366959b55569ede9f77a5307311056e0b74abf6
Justin Rolston <justinrolston@gmail.com>
2012-06-24 15:44:41 -0400

    Init Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NominationTracerApp::Application.config.secret_token = '5d9cefa7ec1754539a9d86883d197f259f21bc7a013dbeb9eeb70b5698e6c1065ca59dc9610bf450b7e7f3c4df729a19798b5c3c9b073187b20181b06b3e83cb'
