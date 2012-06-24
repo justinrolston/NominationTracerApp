0537e538c0845582e9fc923b458e4468fe568d88
Justin Rolston <justinrolston@gmail.com>
2012-06-24 17:29:35 -0400

    added validation

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,2 +1,4 @@
 class Member < ActiveRecord::Base
+  validates :first_name, :last_name, :email, :presence => true
+  validates :email, :format => {:with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i}
 end
