04c568daba2f0ca0a6202a19a7c0d6f7b539c51d
Justin Rolston <justinrolston@gmail.com>
2012-06-25 12:36:04 -0400

    added unique validation

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,4 +1,4 @@
 class Member < ActiveRecord::Base
   validates :first_name, :last_name, :email, :presence => true
-  validates :email, :format => {:with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i}
+  validates :email, :uniqueness => { :case_sensitive => false }, :format => {:with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i}
 end
