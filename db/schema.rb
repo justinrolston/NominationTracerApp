2401b0096b25c24fb705eb71172536153db88464
Justin Rolston <justinrolston@gmail.com>
2012-06-28 19:36:54 -0400

    added ministry

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120625234051) do
+ActiveRecord::Schema.define(:version => 20120628233458) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -73,6 +73,12 @@ ActiveRecord::Schema.define(:version => 20120625234051) do
     t.string   "phone_number"
     t.datetime "created_at",   :null => false
     t.datetime "updated_at",   :null => false
+  end
+
+  create_table "ministries", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
 end
